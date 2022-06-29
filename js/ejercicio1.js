eb72305202ec6c7afa5f096fc1e4729f648829a0
Pereyra Jose <luispereyra14@hotmail.com>
2022-06-29 20:40:10 -0300

    prompt en dos lineas

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,12 +1,13 @@
 //ventana alert
-alert("un mensaje")
+alert("un mensaje");
 
 //texto en pantalla
 document.write("<h5>Hello World</h5>");
 
 //suma de una linea
-document.write("Resultado:"+ (3+5) );
+document.write("Resultado:" + (3 + 5));
 
 //prompt nombre de usuario
-alert( "hola "+ prompt("¿Como te llamas usuario?"));
+var nombre = prompt("¿Como te llamas usuario?");
 
+document.write("hola " + nombre);
