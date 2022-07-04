11f216aaac57a9b4a11e88ba5b2b6222b43a2f18
Pereyra Jose <luispereyra14@hotmail.com>
2022-07-04 18:59:27 -0300

    ejercicio6 terminado

---
+++ js/ejercicio6.js
@@ -0,0 +1,7 @@
+ let numero1 = parseInt(prompt("agrege un numero"));
+ let numero2 = parseInt(prompt("agrege un numero"));
+
+if (numero1 > numero2) {
+    document.write("el numero mayor es " + numero1)
+ } else if (numero1 < numero2) {document.write("el numero es menor a " + numero2)}
+ 
